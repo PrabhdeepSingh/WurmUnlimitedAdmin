56d3736061f117f6c4116f729c5465f1a93fd097
Prabhdeep Singh <pssingh0101@gmail.com>
2015-11-05 18:22:13 -0500

    Changed version

--- includes/config.php
+++ includes/config.php
@@ -13,7 +13,7 @@ $application = array(
 	"mode" => DEVELOPMENT,
 	"rootPath" => "//localhost/wurmunlimitedadmin/", // CHANGE ME
 	"minAdminLevel" => 5,
-	"version" => "0.0.8-Alpha"
+	"version" => "0.0.9-Alpha"
 );
 
 /**
